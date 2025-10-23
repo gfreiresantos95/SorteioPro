835f292f9f0d6bed49382fce74fc4cd6af8c5a47
Gabriel Freire <gsantos.ciet@alelo.com.br>
2025-10-22 23:36:52 -0300

    Fixing MainViewModelTest

--- app/src/test/java/com/gabrielfreire/sorteiopro/MainViewModelTest.kt
+++ app/src/test/java/com/gabrielfreire/sorteiopro/MainViewModelTest.kt
@@ -2,36 +2,13 @@ package com.gabrielfreire.sorteiopro
 
 import com.gabrielfreire.sorteiopro.viewmodels.MainViewModel
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceTimeBy
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestWatcher
-import org.junit.runner.Description
-
-@OptIn(ExperimentalCoroutinesApi::class)
-class MainCoroutineRule(
-    val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
-) : TestWatcher() {
-
-    override fun starting(description: Description) {
-        super.starting(description)
-        Dispatchers.setMain(dispatcher)
-    }
-
-    override fun finished(description: Description) {
-        super.finished(description)
-        Dispatchers.resetMain()
-    }
-}
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainViewModelTest {
